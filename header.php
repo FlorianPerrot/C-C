6aaf07adff46719e4d58128f8bde893d694ea27a
Florian perrot <florianperrot0@gmail.com>
2013-04-26 18:47:09 +0200

    Version mi-final du header (25% logo - titre-asso - 75% recherche)

--- header.php
+++ header.php
@@ -18,10 +18,11 @@
 	<body>
 		<div class="wrap">
 			<header>
-				<div class="bande-top"></div>
+				<div class="barre-top"></div>
 				<div class="header-content">
-					<div class="logo"><img alt="logo de culture et cultures" src="<?php echo get_bloginfo('template_directory');?>/img/logo.jpg" width="155px" height="155px"/></div>
 					<img class="titre-asso" alt="Culture et cultures" src="<?php echo get_bloginfo('template_directory');?>/img/titre-asso.gif" width="390px" height="205px"/>
+					
+					<div class="logo"><img alt="logo de culture et cultures" src="<?php echo get_bloginfo('template_directory');?>/img/logo.jpg" width="155px" height="155px"/></div>
                 	<?php get_search_form( $echo ); ?>
                 </div>
 			</header>
